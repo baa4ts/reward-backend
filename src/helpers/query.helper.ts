5e11c3aab192c2424a901758fac2577d45ba8a6e
baa4ts <baa4tsdev@gmail..com>
2026-02-08 12:42:10 -0300

    Update

--- src/helpers/query.helper.ts
+++ src/helpers/query.helper.ts
@@ -1,7 +1,7 @@
 type LimitOptions = { defaultLimit?: number; maxLimit?: number };
 
 export function parseLimit(limitParam: unknown, options: LimitOptions = {}): number {
-    const { defaultLimit = 10, maxLimit = 50 } = options;
+    const { defaultLimit = 9, maxLimit = 54 } = options;
 
     const limit = Math.abs(Number(limitParam));
 
