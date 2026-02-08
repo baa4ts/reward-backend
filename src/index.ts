058d648cda0f99677711e906c5e99dfd3eaa6fbc
baa4ts <baa4tsdev@gmail..com>
2026-02-08 13:13:34 -0300

    Update

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,16 @@ import { Hono } from 'hono'
 import { reward_route } from './api/reward'
 import { compress } from 'hono/compress'
 import { prettyJSON } from 'hono/pretty-json'
+import { cors } from 'hono/cors'
 
 const app = new Hono()
 
-// Global middleware
+// Middleware global
 app.use(compress())
 app.use(prettyJSON())
+
+// CORS
+app.use(cors())
 
 // Rutas
 app.route("/api/v1/rewards", reward_route)
